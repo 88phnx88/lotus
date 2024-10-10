52d9cce425a6e56fd3cfca3dc0bba5c20b6bc0a0
Y <tctctcly@gmail.com>
2024-10-10 18:08:31 +0800

    Update params_calibnet.go
    Propose new time for everythingBurns

--- build/buildconstants/params_calibnet.go
+++ build/buildconstants/params_calibnet.go
@@ -98,8 +98,8 @@ const UpgradeCalibrationDragonFixHeight = 1493854
 // 2024-07-11T12:00:00Z
 const UpgradeWaffleHeight = 1779094
 
-// ???
-const UpgradeEverythingBurnsHeight = math.MaxInt64 - 1
+// 2024-10-23T03:30:00Z
+const UpgradeEverythingBurnsHeight = 2077594
 
 var SupportedProofTypes = []abi.RegisteredSealProof{
 	abi.RegisteredSealProof_StackedDrg32GiBV1,
