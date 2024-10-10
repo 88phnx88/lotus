e40c4e550a498f5aaeed69f9b02fae6cb8fc72ca
Y <tctctcly@gmail.com>
2024-10-10 18:11:00 +0800

    Update params_calibnet.go
    Propose new upgrade time for EverythingBurns

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -103,7 +103,7 @@ const UpgradeCalibrationDragonFixHeight = 1493854
 const UpgradeWaffleHeight = 1779094
 
 // ???
-const UpgradeEverythingBurnsHeight = math.MaxInt64 - 1
+const UpgradeEverythingBurnsHeight = 2077594
 
 var SupportedProofTypes = []abi.RegisteredSealProof{
 	abi.RegisteredSealProof_StackedDrg32GiBV1,
